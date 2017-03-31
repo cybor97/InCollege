0514e7babc2a7385ce047a52f6e3614a56ee90c1
Ruslan <muhametshin_1997@mail.ru>
2017-03-31 22:55:35 +0300

    Implemented HomeHandler

--- InCollege.Server/HomeHandler.cs
+++ InCollege.Server/HomeHandler.cs
@@ -9,7 +9,7 @@ namespace InCollege.Server
         public Task Handle(IHttpContext context, Func<Task> next)
         {
             context.Response = new HttpResponse(HttpResponseCode.Ok, 
-                "<center><b>Добро пожаловать!\nИзвините, но у нас нет веб-интерфейса...</b></center>", 
+                "<center><b>Добро пожаловать!<br/>Извините, но у нас нет веб-интерфейса...<br/>Тебя вообще здесь не должно быть!!!</b></center>", 
                 false);
           
             return Task.Factory.GetCompleted();
