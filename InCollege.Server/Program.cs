5ef14c91ec53df80a9e9d5ebf80e013774a2c24d
Ruslan <muhametshin_1997@mail.ru>
2017-03-31 22:57:14 +0300

    Server part doesn't need WPF GUI. It just needs neetwork interface.

---
+++ InCollege.Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using InCollege.Core.Data.Base;
+using System;
+using System.IO;
+
+namespace InCollege.Server
+{
+    class Program
+    {
+        public static void Main()
+        {
+            DBHolderSQL.Init(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "test.db"));
+            InCollegeServer.Start();
+            while (true) System.Threading.Thread.Sleep(100);
+        }
+    }
+}
