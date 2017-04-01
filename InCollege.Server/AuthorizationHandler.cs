dbe5a3c8e9028c9c46ae937504bdf4fab1d8dfa4
Ruslan <muhametshin_1997@mail.ru>
2017-04-01 21:03:14 +0300

    SignIn and SignUp can be in single handler.

---
+++ InCollege.Server/AuthorizationHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using uhttpsharp;
+
+namespace InCollege.Server
+{
+    class AuthorizationHandler : IHttpRequestHandler
+    {
+        public Task Handle(IHttpContext context, Func<Task> next)
+        {
+            return Task.Factory.GetCompleted();
+        }
+    }
+}
