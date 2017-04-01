f7573609342d9062169420a226820e5fd8e7e4a9
Ruslan <muhametshin_1997@mail.ru>
2017-04-01 21:10:58 +0300

    Made it little more user-friendly... IDK for what, it anyway won't be used.

--- InCollege.Server/HomeHandler.cs
+++ InCollege.Server/HomeHandler.cs
@@ -8,10 +8,14 @@ namespace InCollege.Server
     {
         public Task Handle(IHttpContext context, Func<Task> next)
         {
-            context.Response = new HttpResponse(HttpResponseCode.Ok, 
-                "<center><b>Добро пожаловать!<br/>Извините, но у нас нет веб-интерфейса...<br/>Тебя вообще здесь не должно быть!!!</b></center>", 
+            context.Response = new HttpResponse(HttpResponseCode.Ok,
+                "<center><h1><b>Добро пожаловать</b></h1></center>" +
+                "<center><h2><b>На сервер системы InCollege</b></h2></center>" +
+                "<center><a href=\"/Auth\">Авторизация</a></center>" +
+                "<center><a href=\"/Data?action=GetRange&table=AttestationType\">Данные</a></center>" +
+                "<a style=\"position:fixed; bottom: 0; height: auto; margin - top:40px; width: 100 %; text - align:center\">Made by [CYBOR]</a>",
                 false);
-          
+
             return Task.Factory.GetCompleted();
         }
     }
