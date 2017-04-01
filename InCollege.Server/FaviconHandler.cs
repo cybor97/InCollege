e7bd767b6d1034f087b2c5ca95b394aef64f7654
Ruslan <muhametshin_1997@mail.ru>
2017-04-01 21:05:02 +0300

    Added favicon. For what? Just because I want so :D

---
+++ InCollege.Server/FaviconHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using uhttpsharp;
+
+namespace InCollege.Server
+{
+    internal class FaviconHandler : IHttpRequestHandler
+    {
+        public Task Handle(IHttpContext context, Func<Task> next)
+        {
+            using (var stream = new MemoryStream())
+            {
+                Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location).ToBitmap().Save(stream, ImageFormat.Png);
+                context.Response = new HttpResponse(HttpResponseCode.Ok, stream.GetBuffer(), true);
+            }
+            return Task.Factory.GetCompleted();
+        }
+    }
+}
