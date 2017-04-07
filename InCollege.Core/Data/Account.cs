1b5a873575eca5863a7ee16908874010309cae8a
Ruslan <muhametshin_1997@mail.ru>
2017-04-07 20:52:11 +0300

    Fixed bug with age validation.

--- InCollege.Core/Data/Account.cs
+++ InCollege.Core/Data/Account.cs
@@ -25,7 +25,7 @@ namespace InCollege.Core.Data
         public virtual int AccountDataID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public AccountType AccountType { get; set; }
+        public int AccountType { get; set; }
         public virtual DateTime BirthDate { get; set; }
         public virtual byte[] ProfileImage { get; set; }
         public string FullName { get; set; }
@@ -42,7 +42,7 @@ namespace InCollege.Core.Data
                 password.Length < 5 ? AccountValidationResult.PasswordTooShort :
                 birthDate == null ? AccountValidationResult.BirthDateUndefined :
                 age.Equals(TimeSpan.Zero) ? AccountValidationResult.BirthDateAfterNow :
-                (decimal)age.TotalDays * DaysInAYear > MaxAge ? AccountValidationResult.AgeTooBig :
+                (decimal)age.TotalDays / DaysInAYear > MaxAge ? AccountValidationResult.AgeTooBig :
                 AccountValidationResult.OK;
         }
 
