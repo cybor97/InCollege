fbef1fb9e67fd25466b9f8445ef8c0497449d5da
Ruslan <muhametshin_1997@mail.ru>
2017-04-07 20:53:51 +0300

    Partly implemented data protection(access management). Implemented data saving. Implemented data removing.

--- InCollege.Server/DataHandler.cs
+++ InCollege.Server/DataHandler.cs
@@ -1,4 +1,5 @@
-﻿using InCollege.Core.Data.Base;
+﻿using InCollege.Core.Data;
+using InCollege.Core.Data.Base;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -15,30 +16,63 @@ namespace InCollege.Server
             var request = context.Request;
             //FIXME:Change to POST
             if (request.Method == HttpMethods.Get)
-                if (request.QueryString.TryGetByName("action", out string action))
-                    context.Response = new HttpResponse(HttpResponseCode.Ok, Actions[action]?.Invoke(request.QueryString), false);
-                else context.Response = new HttpResponse(HttpResponseCode.MethodNotAllowed, string.Empty, true);
+                if (request.QueryString.TryGetByName("token", out string tokenString))
+                {
+                    var validationResult = AuthorizationHandler.VerifyToken(tokenString);
+                    if (validationResult.valid)
+                        if (request.QueryString.TryGetByName("action", out string action))
+                            context.Response = Actions[action]?.Invoke(request.QueryString, validationResult.account);
+                        else context.Response = new HttpResponse(HttpResponseCode.MethodNotAllowed, "Эм.. что от меня требуется???", false);
+                }
+                else context.Response = new HttpResponse(HttpResponseCode.Ok, "Доступ запрещен! Нужен токен!", false);
+            else context.Response = new HttpResponse(HttpResponseCode.MethodNotAllowed, "Метод недоступен!", false);
+
             return Task.Factory.GetCompleted();
         }
 
-        static Dictionary<string, Func<IHttpHeaders, string>> Actions = new Dictionary<string, Func<IHttpHeaders, string>>()
+        static Dictionary<string, Func<IHttpHeaders, Account, HttpResponse>> Actions = new Dictionary<string, Func<IHttpHeaders, Account, HttpResponse>>()
         {
             { "GetRange", GetRangeProcessor },
+            { "Save", SaveProcessor },
+            { "Remove", RemoveProcessor }
         };
 
-        static string GetRangeProcessor(IHttpHeaders query)
+        static HttpResponse GetRangeProcessor(IHttpHeaders query, Account account)
         {
             int skip = query.TryGetByName("skipRecords", out int skipResult) ? skipResult : 0;
             int count = query.TryGetByName("countRecords", out int countResult) ? countResult : -1;
             string column = query.TryGetByName("column", out string columnResult) ? columnResult : null;
             bool fixedString = query.TryGetByName("fixedString", out bool fixedStringResult) && fixedStringResult;
-            List<Tuple<string, object>> whereParams = new List<Tuple<string, object>>();
+            var whereParams = new List<(string, object)>();
             foreach (var current in query)
                 if (current.Key.StartsWith("where"))
-                    whereParams.Add(new Tuple<string, object>(current.Key.Split(new[] { "where" }, StringSplitOptions.RemoveEmptyEntries)[0], current.Value));
-            return query.TryGetByName("table", out string table) ?
-                JsonConvert.SerializeObject(DBHolderSQL.GetRange(table, column, skip, count, fixedString, whereParams.ToArray()), Formatting.Indented) :
-                null;
+                    whereParams.Add((current.Key.Split(new[] { "where" }, StringSplitOptions.RemoveEmptyEntries)[0], current.Value));
+            return new HttpResponse(HttpResponseCode.Ok,
+                query.TryGetByName("table", out string table) ?
+                JsonConvert.SerializeObject(DBHolderSQL.GetRange(table, column, skip, count, fixedString, whereParams.ToArray()), Formatting.Indented).Replace("\n", "<br>") :
+                "Ошибка! Таблица не найдена!", false);
+        }
+
+        static HttpResponse SaveProcessor(IHttpHeaders query, Account account)
+        {
+            if (query.TryGetByName("table", out string table))
+            {
+                var fields = new List<(string, object)>();
+                foreach (var current in query)
+                    if (current.Key.StartsWith("field"))
+                        fields.Add((current.Key.Split(new[] { "field" }, StringSplitOptions.RemoveEmptyEntries)[0], current.Value));
+                return new HttpResponse(HttpResponseCode.Ok, DBHolderSQL.Save(table, fields.ToArray()).ToString(), false);
+            }
+            return new HttpResponse(HttpResponseCode.BadRequest, "Куда сохранять???", false);
+        }
+
+        static HttpResponse RemoveProcessor(IHttpHeaders query, Account account)
+        {
+            if (query.TryGetByName("table", out string table))
+                if (query.TryGetByName("id", out int id))
+                    return new HttpResponse(HttpResponseCode.Ok, DBHolderSQL.Remove(table, id).ToString(), false);
+                else return new HttpResponse(HttpResponseCode.BadRequest, "Откуда удалять???", false);
+            else return new HttpResponse(HttpResponseCode.BadRequest, "Что удалять???", false);
         }
     }
 }
