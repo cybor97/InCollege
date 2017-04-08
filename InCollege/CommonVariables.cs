b7dddb4759815d5da7ec0636159ab31876d72b32
cybor <muhametshin.1997@gmail.com>
2017-04-09 01:35:26 +0300

    Added common variables file to store global common variables(captain Obvilious is here!)

---
+++ InCollege/CommonVariables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace InCollege
+{
+    class CommonVariables
+    {
+        public const string APP_NAME = "InCollege";
+        public static readonly string DataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), APP_NAME);
+    }
+}
