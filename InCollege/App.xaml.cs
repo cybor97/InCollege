8f0d76535857baf6465421a87ab2a5630492c58a
cybor <muhametshin.1997@gmail.com>
2017-04-10 19:30:39 +0300

    App style don't have to be in same file with app startpoint.

---
+++ InCollege/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using InCollege.Client.UI;
+using System.Windows;
+
+namespace InCollege.Client
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainWindow mainWindow = new MainWindow();
+
+            MainWindow = mainWindow;
+
+            var loginWindow = new AuthorizationWindow();
+            loginWindow.ShowDialog();
+            if (loginWindow.DialogResult.HasValue && loginWindow.DialogResult.Value)
+                mainWindow.ShowDialog();
+            else Shutdown();
+        }
+    }
+}
