b02800c264a527023ccdc755e585293b348d249d
cybor <muhametshin.1997@gmail.com>
2017-04-10 19:34:19 +0300

    Created ClientConfiguration class to store client settings.

---
+++ InCollege/ClientConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace InCollege
+{
+    public static class ClientConfiguration
+    {
+        public static string HostName { get; set; } = "localhost";
+        public static int Port { get; set; } = 80;
+    }
+}
