b01db0de8ab1c53dbd12a94c709e6a98dcf72318
cybor <muhametshin.1997@gmail.com>
2017-04-10 19:33:33 +0300

    Partly implemented simpliest menu items.

--- InCollege/UI/MainWindow.xaml.cs
+++ InCollege/UI/MainWindow.xaml.cs
@@ -1,12 +1,23 @@
 ﻿using System.Windows;
 
-namespace InCollege.Client
+namespace InCollege.Client.UI
 {
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void ExitItem_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DictionariesItem_Click(object sender, RoutedEventArgs e)
+        {
+            new DictionariesWindow().ShowDialog();
         }
     }
 }
