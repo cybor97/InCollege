c04ddb6a23c505ab48fbe2a0601f57a8741db039
Ruslan <muhametshin_1997@mail.ru>
2017-04-11 09:29:11 +0300

    Created blanks for InCollege.Installer

---
+++ InCollege.Installer/LNKManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InCollege.Installer
+{
+    public static class LNKManager
+    {
+        public static void CreateLinks(params (string filePath, string linkPath)[] links)
+        {
+
+        }
+
+        public static void RemoveLinks(params string[] links)
+        {
+
+        }
+    }
+}
---
+++ InCollege.Installer/RegistryManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InCollege.Installer
+{
+    class RegistryManager
+    {
+        public static void CreateApplicationEntry()
+        {
+
+        }
+
+        public static void RemoveApplicationEntry()
+        {
+
+        }
+    }
+}
