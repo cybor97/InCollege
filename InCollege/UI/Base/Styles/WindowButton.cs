544798f58959c184e46811e6b0690a136153c9f3
cybor <muhametshin.1997@gmail.com>
2017-04-22 11:03:04 +0300

    Created button class with "AnimationColor" property to create custom buttons with different animations.

---
+++ InCollege/UI/Base/Styles/WindowButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace InCollege.UI.Base.Styles
+{
+    public class WindowButton : Button
+    {
+        public static DependencyProperty AnimateColorProperty = DependencyProperty.Register("AnimateColor", typeof(Color?), typeof(WindowButton),
+            new FrameworkPropertyMetadata(Colors.Black,
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public Color? AnimateColor
+        {
+            get
+            {
+                return (Color?)GetValue(AnimateColorProperty);
+            }
+
+            set
+            {
+                SetValue(AnimateColorProperty, value);
+            }
+        }
+    }
+}
