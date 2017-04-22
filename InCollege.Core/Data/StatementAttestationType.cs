354232fed6b00f4bf278a7253a0a28a8410fadda
cybor <muhametshin.1997@gmail.com>
2017-04-22 11:22:01 +0300

    Created required classes to match DB structure.

---
+++ InCollege.Core/Data/StatementAttestationType.cs
@@ -0,0 +1,10 @@
+﻿using InCollege.Core.Data.Base;
+
+namespace InCollege.Core.Data
+{
+    class StatementAttestationType:DBRecord
+    {
+        public int AttestationTypeID { get; set; }
+        public int StatementID { get; set; }
+    }
+}
---
+++ InCollege.Core/Data/Teacher.cs
@@ -0,0 +1,10 @@
+﻿using InCollege.Core.Data.Base;
+
+namespace InCollege.Core.Data
+{
+    public class Teacher : DBRecord
+    {
+        public int ProfessorID { get; set; }
+        public int SubjectID { get; set; }
+    }
+}
