368845d9eb2cf9e897058efbfefa25c1869282ef
cybor <muhametshin.1997@gmail.com>
2017-04-22 11:08:50 +0300

    Created UI- and authorization logic.

--- InCollege/App.xaml.cs
+++ InCollege/App.xaml.cs
@@ -1,24 +1,65 @@
 ﻿using InCollege.Client.UI;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace InCollege.Client
 {
     public partial class App : Application
     {
+        static string _token = null;
+        public static string Token
+        {
+            get
+            {
+                if (_token == null && File.Exists(CommonVariables.TokenFileName))
+                    _token = File.ReadAllText(CommonVariables.TokenFileName);
+                return _token;
+            }
+            set
+            {
+                if (!Directory.Exists(CommonVariables.DataDirectory))
+                    Directory.CreateDirectory(CommonVariables.DataDirectory);
+                _token = value;
+                if (value != null)
+                    File.WriteAllText(CommonVariables.TokenFileName, _token);
+                else
+                    File.Delete(CommonVariables.TokenFileName);
+            }
+        }
 
         public App()
         {
             InitializeComponent();
 
-            MainWindow mainWindow = new MainWindow();
-
-            MainWindow = mainWindow;
+            MainWindow = new MainWindow();
 
             var loginWindow = new AuthorizationWindow();
-            loginWindow.ShowDialog();
-            if (loginWindow.DialogResult.HasValue && loginWindow.DialogResult.Value)
-                mainWindow.ShowDialog();
+            if (Token != null && ValidateToken() ||
+                loginWindow.ShowDialog().HasValue && loginWindow.DialogResult.Value)
+                MainWindow.ShowDialog();
             else Shutdown();
+        }
+
+        static bool ValidateToken()
+        {
+            try
+            {
+                Task<HttpResponseMessage> task = new HttpClient()
+                                                 .PostAsync($"http://{ClientConfiguration.HostName}:{ClientConfiguration.Port}/Auth",
+                                                 new StringContent($"Action=ValidateToken&token={Token}"));
+                task.Wait();
+                return task.Result.StatusCode == HttpStatusCode.OK;
+            }
+            catch (AggregateException e)
+            {
+                //UNDONE:Remove this debug staff!
+                MessageBox.Show(e.ToString());
+                return true;
+            }
         }
     }
 }
