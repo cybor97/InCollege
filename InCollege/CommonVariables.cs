5d37d1174af6890abc9e5b56c09d06ad2bb04961
cybor <muhametshin.1997@gmail.com>
2017-04-22 11:09:23 +0300

    Created some common variables for future work.

--- InCollege/CommonVariables.cs
+++ InCollege/CommonVariables.cs
@@ -7,5 +7,7 @@ namespace InCollege
     {
         public const string APP_NAME = "InCollege";
         public static readonly string DataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), APP_NAME);
+        public static readonly string DBFileName = Path.Combine(DataDirectory, "InCollege.db");
+        public static readonly string TokenFileName = Path.Combine(DataDirectory, "Account.token");
     }
 }
