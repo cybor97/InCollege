1dfa5e6c16f1fad870bc518eee9b4eba4b39eddd
cybor <muhametshin.1997@gmail.com>
2017-04-22 11:04:07 +0300

    Created converter to fetch image path by it's name.

---
+++ InCollege/UI/Util/ImageToSourceConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace InCollege.UI.Util
+{
+    class ImageToSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string)
+                return new BitmapImage(new Uri($"pack://application:,,,/InCollege.Core;component/Resources/{value}.png"));
+            else return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
