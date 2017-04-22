df8d5baa9352c62d932a4b84cdf4d21c8a0ad253
cybor <muhametshin.1997@gmail.com>
2017-04-22 11:10:52 +0300

    Created task for future work.

---
+++ InCollege/SyncManager.cs
@@ -0,0 +1,11 @@
+﻿namespace InCollege
+{
+    public static class SyncManager
+    {
+        //TODO:Implement
+        //public static (bool success, string resultCode) SyncDB()
+        //{
+
+        //}
+    }
+}
