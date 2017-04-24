9e4311aaf3c30f689e4d0d79656c8473930f5fd8
cybor <muhametshin.1997@gmail.com>
2017-04-24 11:01:35 +0300

    Created StatementTypeString property to display statement type name in human-readable form.

--- InCollege.Core/Data/Statement.cs
+++ InCollege.Core/Data/Statement.cs
@@ -1,9 +1,10 @@
 ﻿using InCollege.Core.Data.Base;
+using SQLite;
 using System;
 
 namespace InCollege.Core.Data
 {
-    public class Statement:DBRecord
+    public class Statement : DBRecord
     {
         public int SubjectID { get; set; }
         public int GroupID { get; set; }
@@ -12,5 +13,24 @@ namespace InCollege.Core.Data
         public int Semester { get; set; }
         public virtual DateTime StatementDate { get; set; }
         public virtual string Note { get; set; }
+
+        [Ignore]
+        public string StatementTypeString
+        {
+            get
+            {
+                switch (StatementType)
+                {
+                    case StatementType.MiddleStatement:
+                        return "Промежуточная ведомость";
+                    case StatementType.ExamStatement:
+                        return "Экзаменационная ведомость";
+                    case StatementType.TotalStatement:
+                        return "Сводная ведомость";
+                    default:
+                        return "Не определено :(";
+                }
+            }
+        }
     }
 }
