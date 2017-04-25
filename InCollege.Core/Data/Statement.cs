b45962bc59c030d183a8184d4fc3089ecb945c65
cybor <muhametshin.1997@gmail.com>
2017-04-25 10:06:44 +0300

    Added display data, that is user-readable representation of real data. Added service data, that will be used for in-class purposes. Added local data, that will be used for local(not storing in DB or sending through network) work.

--- InCollege.Core/Data/Statement.cs
+++ InCollege.Core/Data/Statement.cs
@@ -1,6 +1,7 @@
 ﻿using InCollege.Core.Data.Base;
 using SQLite;
 using System;
+using System.Collections.Generic;
 
 namespace InCollege.Core.Data
 {
@@ -14,23 +15,47 @@ namespace InCollege.Core.Data
         public virtual DateTime StatementDate { get; set; }
         public virtual string Note { get; set; }
 
+        #region Display data
         [Ignore]
         public string StatementTypeString
         {
             get
             {
-                switch (StatementType)
-                {
-                    case StatementType.MiddleStatement:
-                        return "Промежуточная ведомость";
-                    case StatementType.ExamStatement:
-                        return "Экзаменационная ведомость";
-                    case StatementType.TotalStatement:
-                        return "Сводная ведомость";
-                    default:
-                        return "Не определено :(";
-                }
+                return StatementTypeStrings[StatementType];
             }
         }
+        #endregion
+
+        #region Local data
+        [Ignore]
+        public List<AttestationType> AttestationTypes { get; set; }
+
+        [Ignore]
+        public List<CommissionMember> CommissionMembers { get; set; }
+
+        //Either ExamStatementResult either MiddleStatementResult. Depends of StatementType.
+        [Ignore]
+        public List<DBRecord> StatementResults { get; set; }
+
+        [Ignore]
+        public Group Group { get; set; }
+
+        [Ignore]
+        public Subject Subject { get; set; }
+        #endregion
+
+        #region Service data
+        static readonly Dictionary<StatementType, string> StatementTypeStrings = new Dictionary<StatementType, string>()
+        {
+            { StatementType.Middle, "Промежуточная" },
+            { StatementType.Exam, "Экзаменационная" },
+            { StatementType.QualificationExam, "Протокол квалификационного экзамена" },
+            { StatementType.StudyPractice, "Учебная практика" },
+            { StatementType.IndustrialPractice, "Производственная практика" },
+            { StatementType.CourceProject, "Курсовой проект" },
+            { StatementType.Total, "Сводная" },
+            { StatementType.Other, "Прочее" }
+        };
+        #endregion
     }
 }
