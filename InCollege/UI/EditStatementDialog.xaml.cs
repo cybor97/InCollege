935b6cee22b192cc60431db0ef2066c2d96481fc
Ruslan <muhametshin_1997@mail.ru>
2017-04-27 02:21:45 +0300

    Created EditStatementDialog UI.

---
+++ InCollege/UI/EditStatementDialog.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace InCollege.UI
+{
+    public partial class EditStatementDialog : Grid
+    {
+        public event RoutedEventHandler OnSave;
+        public event RoutedEventHandler OnCancel;
+
+        public EditStatementDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            OnSave?.Invoke(sender, e);
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            OnCancel?.Invoke(sender, e);
+        }
+    }
+}
