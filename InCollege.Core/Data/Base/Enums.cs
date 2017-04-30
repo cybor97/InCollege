f9dbd105b13f91651b832dbf52667b61c3d337ef
cybor <muhametshin.1997@gmail.com>
2017-04-30 14:10:20 +0300

    I was an idiot when made this unneeded offset...

--- InCollege.Core/Data/Base/Enums.cs
+++ InCollege.Core/Data/Base/Enums.cs
@@ -14,11 +14,11 @@
 
     public enum AccountType : byte
     {
-        Guest = 1,
-        Student = 2,
-        Professor = 3,
-        DepartmentHead = 4,
-        Admin = 5
+        Guest,
+        Student,
+        Professor,
+        DepartmentHead,
+        Admin
     }
 
     public enum TechnicalMarkValue : sbyte
