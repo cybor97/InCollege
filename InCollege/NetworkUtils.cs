a095aaaf292529f90105fc6af7442cf17116d4b9
cybor <muhametshin.1997@gmail.com>
2017-05-04 01:18:03 +0300

    Created some new tools.

--- InCollege/NetworkUtils.cs
+++ InCollege/NetworkUtils.cs
@@ -1,9 +1,11 @@
 ﻿using InCollege.Client;
+using InCollege.Core.Data;
 using InCollege.Core.Data.Base;
 using InCollege.UI;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -19,17 +21,40 @@ namespace InCollege
 
     public class NetworkUtils
     {
-        public static async Task<IEnumerable<T>> RequestData<T>(Window context) where T : DBRecord
+        public static async Task<Account> WhoAmI()
+        {
+            HttpResponseMessage response;
+            if ((response = (await new HttpClient()
+            .PostAsync(ClientConfiguration.AuthHandlerPath,
+            new StringContent(
+            $"Action=WhoAmI&" +
+            $"token={App.Token}")))).StatusCode == HttpStatusCode.OK)
+                return JsonConvert.DeserializeObject<IList<Account>>(await response.Content.ReadAsStringAsync())[0];
+            else
+                MessageBox.Show(await response.Content.ReadAsStringAsync());
+            return null;
+        }
+
+        public static async Task<IEnumerable<T>> RequestData<T>(Window context, params (string name, object value)[] whereParams) where T : DBRecord
         {
             try
             {
                 HttpResponseMessage response;
+
+                var whereString = string.Join("&", whereParams.Select(c => $"where{c.name}=" + WebUtility.UrlEncode(((c.value is byte[]) ? Convert.ToBase64String((byte[])c.value) :
+                                                        (c.value is bool) ? ((bool)c.value ? 1 : 0) :
+                                                        (c.value is Enum) ? (byte)c.value :
+                                                        (c.value is DateTime) ? ((DateTime)c.value).ToString("yyyy-MM-dd") :
+                                                        c.value).ToString())));
+
                 if ((response = (await new HttpClient()
-                      .PostAsync($"http://{ClientConfiguration.HostName}:{ClientConfiguration.Port}/Data",
+                      .PostAsync(ClientConfiguration.DataHandlerPath,
                       new StringContent(
                       $"Action=GetRange&" +
                       $"table={typeof(T).Name}&" +
-                      $"token={App.Token}")))).StatusCode == HttpStatusCode.OK)
+                      $"token={App.Token}" +
+                      //Not an error! Little more attension, & is  ' here
+                      (!string.IsNullOrWhiteSpace(whereString) ? $"&{whereString}" : ""))))).StatusCode == HttpStatusCode.OK)
                     return JsonConvert
                         .DeserializeObject<IEnumerable<T>>(await response.Content.ReadAsStringAsync());
                 else
@@ -51,7 +76,7 @@ namespace InCollege
             {
                 HttpResponseMessage response;
                 if ((response = (await new HttpClient()
-                      .PostAsync($"http://{ClientConfiguration.HostName}:{ClientConfiguration.Port}/Data",
+                      .PostAsync(ClientConfiguration.DataHandlerPath,
                       new StringContent(
                       $"Action={Enum.GetName(typeof(DataAction), action)}&" +
                       $"table={typeof(T).Name}&" +
@@ -71,5 +96,7 @@ namespace InCollege
             }
             return "-1";
         }
+
+
     }
 }
