a2e84c53267481a8e9addbc7ba567127979376f3
cybor <muhametshin.1997@gmail.com>
2017-05-04 11:21:09 +0300

    Removed tons of indian code.

--- InCollege/UI/StudyObjectsWindow.xaml.cs
+++ InCollege/UI/StudyObjectsWindow.xaml.cs
@@ -1,7 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using InCollege.Core.Data;
+using InCollege.Core.Data.Base;
+using MaterialDesignThemes.Wpf;
 using System.Collections.Generic;
-using System.Net;
-using System.Net.Http;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,43 +11,38 @@ namespace InCollege.UI
 {
     public partial class StudyObjectsWindow : Window
     {
+        Dictionary<object, DialogHost> Dialogs;
+
         public StudyObjectsWindow(int tabIndex)
         {
             InitializeComponent();
             StudyObjectsTabs.SelectedIndex = tabIndex;
+
+            Dialogs = new Dictionary<object, DialogHost>
+            {
+                { "Department", EditDepartmentDialog },
+                { "Specialty", EditSpecialtyDialog },
+                { "Group", EditGroupDialog },
+                { "Subject", EditSubjectDialog }
+            };
         }
 
         async Task UpdateDisplayData()
         {
             Title = ((TabItem)StudyObjectsTabs.SelectedItem).Header.ToString();
 
-            //try
-            //{
-            //    StatementsLV.Items.Clear();
-            //    if ((response = (await new HttpClient()
-            //          .PostAsync($"http://{ClientConfiguration.HostName}:{ClientConfiguration.Port}/Data",
-            //          new StringContent(
-            //          $"Action=GetRange&" +
-            //          $"table={nameof(Statement)}&" +
-            //          $"token={App.Token}")))).StatusCode == HttpStatusCode.OK)
-            //    {
-            //        var result = JsonConvert.DeserializeObject<IEnumerable<Statement>>(await response.Content.ReadAsStringAsync());
-            //        //Yeah, 2 times, to prevent multiplying
-            //        StatementsLV.Items.Clear();
-            //        foreach (var current in result)
-            //            StatementsLV.Items.Add(current);
-            //    }
-            //    else
-            //        MessageBox.Show(await response.Content.ReadAsStringAsync());
-            //}
-            //catch (HttpRequestException exc)
-            //{
-            //    MessageBox.Show($"Ошибка подключения к серверу. Проверьте:\n-запущен ли сервер\n-настройки брандмауэра\n-правильно ли указан адрес\nТехническая информация:\n\n{exc.Message}");
-            //}
+            DepartmentHeadCB.ItemsSource = await NetworkUtils.RequestData<Account>(this, (nameof(Account.AccountType), AccountType.DepartmentHead));
 
+            DepartmentsLV.ItemsSource = await NetworkUtils.RequestData<Department>(this);
+            foreach (Department current in DepartmentsLV.ItemsSource)
+                current.DepartmentHeadName = (await NetworkUtils.RequestData<Account>(this, (nameof(Account.ID), current.DepartmentHeadID)))?.FirstOrDefault()?.FullName;
+
+            SpecialtiesLV.ItemsSource = await NetworkUtils.RequestData<Specialty>(this);
+            GroupsLV.ItemsSource = await NetworkUtils.RequestData<Group>(this);
+            SubjectsLV.ItemsSource = await NetworkUtils.RequestData<Subject>(this);
         }
 
-        async void AccountTypesTabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        async void StudyObjectsTabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             await UpdateDisplayData();
         }
@@ -58,59 +54,24 @@ namespace InCollege.UI
 
         void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            switch (StudyObjectsTabs.SelectedIndex)
-            {
-                case 0:
-                    EditDepartmentDialog.IsOpen = true;
-                    break;
-                case 1:
-                    EditSpecialtyDialog.IsOpen = true;
-                    break;
-                case 2:
-                    EditGroupDialog.IsOpen = true;
-                    break;
-                case 3:
-                    EditSubjectDialog.IsOpen = true;
-                    break;
-            }
+            var dialog = Dialogs[((TabItem)StudyObjectsTabs.SelectedItem).Tag];
+            dialog.IsOpen = true;
+            dialog.DataContext =
+                dialog == EditDepartmentDialog ? new Department() :
+                dialog == EditSpecialtyDialog ? new Specialty() :
+                dialog == EditGroupDialog ? new Group() :
+                dialog == EditSubjectDialog ? (DBRecord)new Subject() :
+                null;
         }
 
         void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            switch (((Button)sender).Tag.ToString())
-            {
-                case "Department":
-                    EditDepartmentDialog.IsOpen = true;
-                    break;
-                case "Specialty":
-                    EditSpecialtyDialog.IsOpen = true;
-                    break;
-                case "Group":
-                    EditGroupDialog.IsOpen = true;
-                    break;
-                case "Subject":
-                    EditSubjectDialog.IsOpen = true;
-                    break;
-            }
+            Dialogs[((Button)sender).Tag].IsOpen = false;
         }
 
         void CancelButton_Click(object sender, RoutedEventArgs e)
         {
-            switch (((Button)sender).Tag.ToString())
-            {
-                case "Department":
-                    EditDepartmentDialog.IsOpen = false;
-                    break;
-                case "Specialty":
-                    EditSpecialtyDialog.IsOpen = false;
-                    break;
-                case "Group":
-                    EditGroupDialog.IsOpen = false;
-                    break;
-                case "Subject":
-                    EditSubjectDialog.IsOpen = false;
-                    break;
-            }
+            Dialogs[((Button)sender).Tag].IsOpen = false;
         }
     }
 }
