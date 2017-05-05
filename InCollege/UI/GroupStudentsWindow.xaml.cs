63757a66a266b2d8e42b5f0b61e82e59fd595c65
cybor <muhametshin.1997@gmail.com>
2017-05-05 23:44:16 +0300

    Partly implemented GroupStudents UI.

---
+++ InCollege/UI/GroupStudentsWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using InCollege.Core.Data;
+using InCollege.Core.Data.Base;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace InCollege.UI
+{
+    //TODO:Implement!
+    public partial class GroupStudentsWindow : Window, IUpdatable
+    {
+        Group Group { get; set; }
+
+        public GroupStudentsWindow(Group group)
+        {
+            InitializeComponent();
+            Group = group;
+            Title = $"Группа \"{group?.GroupName} {group?.GroupCode}\"";
+        }
+
+        public async Task UpdateData()
+        {
+            var accountData = await NetworkUtils.RequestData<Account>(this, (nameof(Account.AccountType), AccountType.Student));
+            StudentsLV.ItemsSource = accountData;
+            StudentCB.ItemsSource = accountData;
+        }
+
+        private void EditItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StudentDialog_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            StudentDialog.DataContext = ((List<Account>)StudentCB.ItemsSource).FirstOrDefault();
+            StudentDialog.IsOpen = true;
+        }
+
+        async void GroupStudentsWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await UpdateData();
+        }
+    }
+}
