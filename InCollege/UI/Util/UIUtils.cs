8fa2d8ae34174c1c62aa8a8ee0c6155e502e9256
Cybor <muhametshin.1997@gmail.com>
2017-05-05 13:08:52 +0300

    Created common UIUtils.

---
+++ InCollege/UI/Util/UIUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace InCollege.UI.Util
+{
+    class UIUtils
+    {
+        public static T FindAncestorOrSelf<T>(DependencyObject obj) where T : DependencyObject
+        {
+            while (obj != null)
+            {
+                if (obj is T objTest)
+                    return objTest;
+                obj = VisualTreeHelper.GetParent(obj);
+            }
+            return null;
+        }
+    }
+}
