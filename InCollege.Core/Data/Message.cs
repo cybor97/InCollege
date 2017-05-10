eae1c4f02c7ceb0888a145766e0d700283efc9c7
cybor <muhametshin.1997@gmail.com>
2017-05-10 09:43:02 +0300

    Added fields for MVVM.

--- InCollege.Core/Data/Message.cs
+++ InCollege.Core/Data/Message.cs
@@ -1,4 +1,6 @@
 ﻿using InCollege.Core.Data.Base;
+using Newtonsoft.Json;
+using SQLite;
 using System;
 
 namespace InCollege.Core.Data
@@ -16,7 +18,42 @@ namespace InCollege.Core.Data
         /// "Data":"SOME_ENCODED_MESS"
         /// }
         /// </summary>
-        public virtual string MessageAttachment { get; set; }
-        public virtual DateTime MessageDate { get; set; }
+        public string MessageAttachment { get; set; }
+        public DateTime MessageDate { get; set; }
+
+        [Ignore]
+        [JsonIgnore]
+        public string SenderName { get => Sender?.FullName; }
+
+        [Ignore]
+        [JsonIgnore]
+        public Account Sender
+        {
+            get => _sender;
+            set
+            {
+                _sender = value;
+                FromID = _sender?.ID ?? -1;
+            }
+        }
+        private Account _sender;
+
+        [Ignore]
+        [JsonIgnore]
+        public string ReceiverName { get => Receiver?.FullName; }
+
+        [Ignore]
+        [JsonIgnore]
+        public Account Receiver
+        {
+            get => _receiver;
+            set
+            {
+                _receiver = value;
+                ToID = _receiver?.ID ?? -1;
+            }
+        }
+        private Account _receiver;
+
     }
 }
