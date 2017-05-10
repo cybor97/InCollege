618252e92469cdbf189a67f3460799f0aff301fe
cybor <muhametshin.1997@gmail.com>
2017-05-10 09:48:01 +0300

    _

--- InCollege/ClientConfiguration.cs
+++ InCollege/ClientConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public static class ClientConfiguration
     {
-        public static string HostName { get; set; } = "localhost";
+        public static string HostName { get; set; } = "192.168.1.2";
         public static int Port { get; set; } = 80;
 
         public static readonly string AuthHandlerPath = $"http://{HostName}:{Port}/Auth";
