df31546aab6b803b51a8054b16070ce7682d2282
cybor <muhametshin.1997@gmail.com>
2017-05-13 00:17:27 +0300

    DB path should be as common variable.

---
+++ InCollege.Server/CommonVariables.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace InCollege.Server
+{
+    class CommonVariables
+    {
+        public static string DBLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "InCollege.Server", "InCollege.db");
+    }
+}
