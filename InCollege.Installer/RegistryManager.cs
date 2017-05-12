641b1463619477b816315d83b7957cab7992ad95
cybor <muhametshin.1997@gmail.com>
2017-05-13 00:13:31 +0300

    Implemented RegistryManager to manage registry entries for app.

--- InCollege.Installer/RegistryManager.cs
+++ InCollege.Installer/RegistryManager.cs
@@ -1,20 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.Win32;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InCollege.Installer
 {
     class RegistryManager
     {
-        public static void CreateApplicationEntry()
-        {
+        public const string WINDOWS_APPS_SUBKEY = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
 
+        public static void CreateApplicationEntry(AppPart part)
+        {
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(WINDOWS_APPS_SUBKEY, true))
+                if (key.GetSubKeyNames().Count(c => c.Equals(part.ID)) == 0)
+                    using (var CreatedKey = key.CreateSubKey(part.ID))
+                        foreach (var current in part.Columns)
+                            CreatedKey.SetValue(current.name, current.value);
+                else
+                {
+                    RemoveApplicationEntry(part.ID);
+                    CreateApplicationEntry(part);
+                }
         }
 
-        public static void RemoveApplicationEntry()
+        public static void RemoveApplicationEntry(string partID)
         {
+            if (!string.IsNullOrWhiteSpace(partID))
+                using (var key = Registry.LocalMachine.OpenSubKey(WINDOWS_APPS_SUBKEY, true))
+                    key.DeleteSubKey(key.GetSubKeyNames().FirstOrDefault(c => c.Equals(partID)), false);
+        }
+
+        public static AppPart GetInstalledEntryInfo(string partName)
+        {
+            using (var key = Registry.LocalMachine.OpenSubKey(WINDOWS_APPS_SUBKEY))
+                if (key.GetSubKeyNames().Count(c => c.Equals(partName)) > 0)
+                    using (var currentKey = key.OpenSubKey(partName))
+                        return AppPart.ImportFromColumns(currentKey.GetValueNames().Select(c => (c, currentKey.GetValue(c))));
+            return null;
 
         }
     }
