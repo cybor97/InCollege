8a91d447e2c922b74380697f57c962eee51fb695
cybor <muhametshin.1997@gmail.com>
2017-05-13 00:15:00 +0300

    _

--- InCollege.Installer/ConfigurationManager.cs
+++ InCollege.Installer/ConfigurationManager.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InCollege.Installer
+﻿namespace InCollege.Installer
 {
     public static class ConfigurationManager
     {
-        public static void CreateConfiguration(string serverPath, string serverPortNumber)
+        public static void CreateConfiguration()
         {
-            
+
         }
     }
 }
