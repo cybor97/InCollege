9f7217a0b6cc617965940c9ca3260ecd06e75f15
cybor <muhametshin.1997@gmail.com>
2017-05-15 00:33:56 +0300

    Implemented StatementCommissionMembers UI.

--- InCollege/UI/StatementsUI/StatementCommissionMembersWindow.xaml.cs
+++ InCollege/UI/StatementsUI/StatementCommissionMembersWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using InCollege.Core.Data;
+using InCollege.Core.Data.Base;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace InCollege.Client.UI.StatementsUI
 {
@@ -13,14 +16,79 @@ namespace InCollege.Client.UI.StatementsUI
             Statement = statement;
         }
 
+        async void StatementCommissionMembersWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await UpdateData();
+        }
+
         public async Task UpdateData()
         {
-            //TODO:Implement!
+            var professorsData = await NetworkUtils.RequestData<Account>(this, (nameof(Account.AccountType), AccountType.Professor));
+            professorsData.AddRange(await NetworkUtils.RequestData<Account>(this, (nameof(Account.AccountType), AccountType.DepartmentHead)));
+
+            var commissionMembersData = await NetworkUtils.RequestData<CommissionMember>(this, (nameof(CommissionMember.StatementID), Statement?.ID ?? -1));
+            foreach (var current in commissionMembersData)
+                current.Professor = professorsData.FirstOrDefault(c => c.ID == current.ProfessorID);
+            CommissionMembersLV.ItemsSource = commissionMembersData;
+
+            ProfessorCB.ItemsSource = professorsData.Where(currentProfessor => !commissionMembersData.Any(c => c.ProfessorID == currentProfessor.ID));
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Sorry, still unimplemented.");
+            if (!ProfessorCB.Items.IsEmpty)
+            {
+                CommissionMemberDialog.DataContext = new CommissionMember { StatementID = Statement?.ID ?? -1 };
+                CommissionMemberDialog.IsOpen = true;
+            }
+
+        }
+
+        private void EditItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (CommissionMembersLV.SelectedItem != null)
+            {
+                CommissionMemberDialog.DataContext = CommissionMembersLV.SelectedItem;
+                CommissionMemberDialog.IsOpen = true;
+            }
+        }
+
+        async void RemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var current in CommissionMembersLV.SelectedItems)
+                await NetworkUtils.ExecuteDataAction<CommissionMember>(null, (DBRecord)current, DataAction.Remove);
+            await UpdateData();
+        }
+
+        async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProfessorCB.SelectedItem != null)
+                await NetworkUtils.ExecuteDataAction<CommissionMember>(this, (DBRecord)CommissionMemberDialog.DataContext, DataAction.Save);
+            CommissionMemberDialog.IsOpen = false;
+        }
+
+        void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            CommissionMemberDialog.IsOpen = false;
+        }
+
+        async void CommissionMemberDialog_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Add:
+                case Key.OemPlus:
+                    AddButton_Click(null, null);
+                    break;
+                case Key.Subtract:
+                case Key.OemMinus:
+                case Key.Delete:
+                    RemoveItem_Click(null, null);
+                    break;
+                case Key.F5:
+                    await UpdateData();
+                    break;
+            }
         }
     }
 }
