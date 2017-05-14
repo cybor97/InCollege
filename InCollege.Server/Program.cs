6954117d08d9dd28da0ea3854664fe6e1f32f65f
cybor <muhametshin.1997@gmail.com>
2017-05-15 00:37:47 +0300

    Added system logo as ASCII-art for server part. Why? Because science is not about "why?", it's about "why not?"!

--- InCollege.Server/Program.cs
+++ InCollege.Server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using InCollege.Core.Data.Base;
+using System;
 
 namespace InCollege.Server
 {
@@ -6,7 +7,23 @@ namespace InCollege.Server
     {
         public static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("######################################################");
+            Console.WriteLine("#                                                    #");
+            Console.WriteLine("#  ##  ##  #     ###  ###  #    #    ###  ####  ###  #");
+            Console.WriteLine("#  ##  ##  #     #    # #  #    #    #    #     #    #");
+            Console.WriteLine("#  ##  # # #     #    # #  #    #    ###  # ##  ###  #");
+            Console.WriteLine("#  ##  #  ##     #    # #  #    #    #    #  #  #    #");
+            Console.WriteLine("#  ##  #  ##     ###  ###  ###  ###  ###  ####  ###  #");
+            Console.WriteLine("#                                                    #");
+            Console.WriteLine("######################################################");
+
+            Console.WriteLine("Welcome to InCollege.Server! Don't hesitate, open http://localhost/ to see what we got!");
+            Console.WriteLine("Made by [CYBOR] = Muhametshin R.A.");
+
+            Console.WriteLine($"Initializing SQLite DB(thanks Frank A. Krueger and other 53 team members for sqlite-net engine) in \n{CommonVariables.DBLocation}\n");
             DBHolderSQL.Init(CommonVariables.DBLocation);
+            Console.WriteLine($"Initializing uHttpSharp server engine(thanks Elad Zelingher and other 6 team members for uHttpSharp engine).");
             InCollegeServer.Start();
             while (true) System.Threading.Thread.Sleep(100);
         }
