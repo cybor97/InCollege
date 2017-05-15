e09722ccda9189ce7d61c4544655f8fcb414b47c
cybor <muhametshin.1997@gmail.com>
2017-05-15 10:28:48 +0300

    We don't need attached frames, will better use StatementResult views.

---
+++ InCollege/UI/StatementsUI/ExamStatementResult.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace InCollege.Client.UI.StatementsUI
+{
+    public partial class ExamStatementResult : UserControl
+    {
+        public ExamStatementResult()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InCollege/UI/StatementsUI/MiddleStatementResult.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace InCollege.Client.UI.StatementsUI
+{
+    public partial class MiddleStatementResult : UserControl
+    {
+        public MiddleStatementResult()
+        {
+            InitializeComponent();
+        }
+    }
+}
