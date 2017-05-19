aa48f1ba33150581fc85f6658a6516b5b6317178
cybor <muhametshin.1997@gmail.com>
2017-05-20 01:32:45 +0300

    Created window for complex statement results.

---
+++ InCollege/UI/StatementsUI/StatementResultsWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using InCollege.Core.Data;
+using System.Windows;
+
+namespace InCollege.Client.UI.StatementsUI
+{
+    public partial class StatementResultsWindow : Window
+    {
+        Statement Statement { get => (Statement)DataContext; set => DataContext = value; }
+
+        public StatementResultsWindow(Statement statement)
+        {
+            InitializeComponent();
+        }
+    }
+}
