1fea9346ab7ba7f9dbc6d6039743d79487c4ea96
cybor <muhametshin.1997@gmail.com>
2017-05-22 10:31:38 +0300

    Partly implemented installer components UI.

--- InCollege.Installer/MainWindow.xaml.cs
+++ InCollege.Installer/MainWindow.xaml.cs
@@ -1,28 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace InCollege.Installer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            ComponentsLV.ItemsSource = Directory.GetFiles(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "*.apm")
+                                                .Select(c => new BinaryFormatter().Deserialize(new FileStream(c, FileMode.Open)));
+        }
+
+        void LogoAnimation_Completed(object sender, EventArgs e)
+        {
+            ((Storyboard)ComponentsLV.Resources["ComponentsLVAppearStoryboard"]).Begin();
+        }
+
+        void SelectedCB_CheckChanged(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
