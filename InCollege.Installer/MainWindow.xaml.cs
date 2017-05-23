b9f45da4afe85e0f7cdaad4b164ff7224a8ea903
cybor <muhametshin.1997@gmail.com>
2017-05-24 00:44:23 +0300

    Implemented Uninstall logic.

--- InCollege.Installer/MainWindow.xaml.cs
+++ InCollege.Installer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.IO.Compression;
@@ -19,7 +20,10 @@ namespace InCollege.Installer
     {
         ResourceSet ResourceSet = Properties.Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentUICulture, true, true);
         List<AppPart> InstallComponents = new List<AppPart>();
-        List<string> ResourceKeys = new List<string>();
+        string MyFileName = Assembly.GetExecutingAssembly().Location;
+        string CurrentDirectory = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName;
+
+        bool UninstallerMode = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -34,11 +38,21 @@ namespace InCollege.Installer
                         InstallComponents.Add((AppPart)new BinaryFormatter().Deserialize(new MemoryStream((byte[])entry.Value)));
             });
 
-            ComponentsLV.ItemsSource = InstallComponents;
+            UninstallerMode = InstallComponents.Any(c => MyFileName.ToLower().Contains(c.UninstallString.Replace("INSTALL_LOCATION", string.Empty).ToLower()));
 
             PreInstallTB.Visibility = Visibility.Collapsed;
             ((Storyboard)InstallButtons.Resources["InstallButtonsAppearStoryboard"]).Begin();
-            ((Storyboard)ComponentsLV.Resources["ComponentsLVAppearStoryboard"]).Begin();
+
+            if (UninstallerMode)
+            {
+                InstallButton.Content = "Удалить";
+                InstallButton.IsEnabled = true;
+            }
+            else
+            {
+                ComponentsLV.ItemsSource = InstallComponents;
+                ((Storyboard)ComponentsLV.Resources["ComponentsLVAppearStoryboard"]).Begin();
+            }
         }
 
         void SelectedCB_CheckChanged(object sender, RoutedEventArgs e)
@@ -47,6 +61,74 @@ namespace InCollege.Installer
         }
 
         async void InstallButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (UninstallerMode)
+                await UninstallApp();
+            else
+                await InstallApp(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "IN_COLLEGE_INSTALLER_TEST"));
+
+            Close();
+        }
+
+        async Task UninstallApp()
+        {
+            PreInstallTB.Visibility = Visibility.Visible;
+            PreInstallTB.Text = "Подготовка к удалению...\nЗакройте все окна приложения";
+
+            await Task.Run(() =>
+            {
+                var myID = Process.GetCurrentProcess().Id;
+                while (Process.GetProcesses().Any(c => c.ProcessName.ToLower().Contains("incollege") && c.Id != myID))
+                    Thread.Sleep(100);
+            });
+
+            foreach (var currentComponent in InstallComponents.Select(c => RegistryManager.GetInstalledEntryInfo(c.ID)))
+                if (currentComponent != null)
+                {
+                    var files = Directory.GetFiles(CurrentDirectory).Where(c => c.EndsWith("APM")).ToArray();
+                    if (files.Length > 0)
+                    {
+                        AppPart localComponent = null;
+                        await Task.Run(() =>
+                        {
+                            while (true)
+                                try
+                                {
+                                    localComponent = ((AppPart)new BinaryFormatter().Deserialize(new FileStream(files[0], FileMode.Open)));
+                                    break;
+                                }
+                                catch (IOException)
+                                {
+                                    Thread.Sleep(100);
+                                }
+                        });
+                        if (currentComponent.ID == localComponent.ID)
+                        {
+                            PreInstallTB.Text = $"Удаление \"{currentComponent.DisplayName}\"...";
+                            await Task.Run(() =>
+                            {
+                                foreach (var current in Directory.GetFiles(currentComponent.InstallLocation))
+                                    if (current != MyFileName)
+                                        try
+                                        {
+                                            File.Delete(current);
+                                        }
+                                        catch (IOException) { }
+
+                                RegistryManager.RemoveApplicationEntry(currentComponent.ID);
+                                LNKManager.RemoveLinks(Path.Combine(CommonVariables.DesktopPath, $"{currentComponent.DisplayName}.lnk"));
+                                Process.Start(new ProcessStartInfo("cmd", $" /C timeout 10&&rd /s /q \"{CurrentDirectory}\"") { CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden });
+                            });
+                            break;
+                        }
+                    }
+                }
+            PreInstallTB.Text = "Удаление успешно завершено!";
+            await Task.Run(() => Thread.Sleep(2000));
+            Process.GetCurrentProcess().Kill();
+        }
+
+        async Task InstallApp(string installRoot)
         {
             PreInstallTB.Visibility = Visibility.Visible;
             PreInstallTB.Text = "Идет установка ПО...";
@@ -57,15 +139,15 @@ namespace InCollege.Installer
             ((Storyboard)InstallButtons.Resources["InstallButtonsDisappearStoryboard"]).Begin();
             ((Storyboard)ComponentsLV.Resources["ComponentsLVDisappearStoryboard"]).Begin();
 
-
+            if (!Directory.Exists(CommonVariables.TMPDirPath)) Directory.CreateDirectory(CommonVariables.TMPDirPath);
             foreach (var currentComponent in InstallComponents)
                 if (currentComponent.Selected)
                 {
-                    PreInstallTB.Text = $"Установка {currentComponent.DisplayName}...";
+                    PreInstallTB.Text = $"Установка \"{currentComponent.DisplayName}\"...";
                     await Task.Run(() =>
                     {
                         currentComponent.InstallLocation = currentComponent.InstallLocation
-                            .Replace("INSTALL_ROOT", Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "IN_COLLEGE_INSTALLER_TEST"));
+                            .Replace("INSTALL_ROOT", installRoot);
 
                         currentComponent.ExecutablePath = currentComponent.ExecutablePath
                             .Replace("INSTALL_LOCATION", currentComponent.InstallLocation);
@@ -76,7 +158,6 @@ namespace InCollege.Installer
                         currentComponent.UninstallString = currentComponent.UninstallString
                             .Replace("INSTALL_LOCATION", currentComponent.InstallLocation);
 
-                        if (!Directory.Exists(CommonVariables.TMPDirPath)) Directory.CreateDirectory(CommonVariables.TMPDirPath);
                         var tmpFilename = Path.Combine(CommonVariables.TMPDirPath, currentComponent.ID);
                         File.WriteAllBytes(tmpFilename, (byte[])ResourceSet.GetObject(currentComponent.ID.Replace('.', '_')));
 
@@ -85,17 +166,35 @@ namespace InCollege.Installer
                         foreach (var current in zip.ReadCentralDir())
                             if (!current.FilenameInZip.EndsWith("\\") && !current.FilenameInZip.EndsWith("/"))
                                 zip.ExtractFile(current, Path.Combine(currentComponent.InstallLocation, current.FilenameInZip));
+                        zip.Close();
 
-                        File.Copy(Assembly.GetExecutingAssembly().Location, currentComponent.UninstallString, true);
+                        var componentMarkupResourceName = $"{currentComponent.ID.Replace('.', '_')}_APM";
+                        File.WriteAllBytes(Path.Combine(Directory.GetParent(currentComponent.UninstallString).FullName, componentMarkupResourceName),
+                            (byte[])((DictionaryEntry)ResourceSet.OfType<object>().FirstOrDefault(c => ((DictionaryEntry)c).Key.ToString() == componentMarkupResourceName)).Value);
+                        File.Copy(MyFileName, currentComponent.UninstallString, true);
 
                         RegistryManager.CreateApplicationEntry(currentComponent);
                         LNKManager.CreateLinks(currentComponent, Path.Combine(CommonVariables.DesktopPath, $"{currentComponent.DisplayName}.lnk"));
                     });
                 }
 
+            PreInstallTB.Text = "Удаление временных файлов...";
+            await Task.Run(() =>
+            {
+                while (true)
+                    try
+                    {
+                        Directory.Delete(CommonVariables.TMPDirPath, true);
+                        break;
+                    }
+                    catch (IOException)
+                    {
+                        Thread.Sleep(100);
+                    }
+            });
+
             PreInstallTB.Text = "Установка успешно завершена!";
             await Task.Run(() => Thread.Sleep(2000));
-            Close();
         }
 
         void CancelButton_Click(object sender, RoutedEventArgs e)
