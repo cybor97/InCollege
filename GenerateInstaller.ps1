437373d34856d8ff3c017418b3e54151c4538b68
cybor <muhametshin.1997@gmail.com>
2017-05-24 00:45:11 +0300

    Fixed installer build script(yeah, my own little Gradle :D).

--- GenerateInstaller.ps1
+++ GenerateInstaller.ps1
@@ -1,5 +1,6 @@
+MSBuild
 Compress-Archive -Path .\InCollege\bin\Debug\* -DestinationPath .\InCollege.Installer\Resources\InCollege_Client -Update
 mv .\InCollege.Installer\Resources\InCollege_Client.zip .\InCollege.Installer\Resources\InCollege_Client -force
 Compress-Archive -Path .\InCollege.Server\bin\Debug\* -DestinationPath .\InCollege.Installer\Resources\InCollege_Server -Update
 mv .\InCollege.Installer\Resources\InCollege_Server.zip .\InCollege.Installer\Resources\InCollege_Server -force
-MSBuild
+MSBuild .\InCollege.Installer\InCollege.Installer.csproj
