0a1cd188457b65656328b16f871dacc69abf24e7
cybor <muhametshin.1997@gmail.com>
2017-05-26 19:56:30 +0300

    Added properties for MVVM.

--- InCollege.Core/Data/RePass.cs
+++ InCollege.Core/Data/RePass.cs
@@ -1,14 +1,37 @@
 ﻿using InCollege.Core.Data.Base;
+using Newtonsoft.Json;
+using SQLite;
 using System;
 
 namespace InCollege.Core.Data
 {
     public class RePass : DBRecord
     {
+        public int StatementID { get; set; }
         public int StatementResultID { get; set; }
         public int TeacherID { get; set; }
         public int RePassNumber { get; set; }
-        public int MarkValue { get; set; }
+        public sbyte MarkValue { get; set; }
         public DateTime RePassDate { get; set; }
+
+        [Ignore]
+        [JsonIgnore]
+        public string MarkValueString
+        {
+            get => StatementResult.GetMarkValueString(MarkValue);
+        }
+
+        [Ignore]
+        [JsonIgnore]
+        public Teacher Teacher
+        {
+            get => _teacher;
+            set
+            {
+                _teacher = value;
+                TeacherID = value?.ID ?? -1;
+            }
+        }
+        private Teacher _teacher;
     }
 }
