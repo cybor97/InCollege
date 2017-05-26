858c103b9a03aeb65564b759d5055be33ca22734
cybor <muhametshin.1997@gmail.com>
2017-05-26 20:00:24 +0300

    Implemented statement generation. Fixed bugs. Made refactoring.

--- InCollege/UI/MainUI/MainWindow.xaml.cs
+++ InCollege/UI/MainUI/MainWindow.xaml.cs
@@ -47,6 +47,7 @@ namespace InCollege.Client.UI.MainUI
             //if(teacher) request<statement>(teacher.subjects);
             //else if (departmentHead) request<statement>(departmentHead.groups);
             //else if (admin) request<statement>(all);
+            //On server side!11
 
             var statementsData = await NetworkUtils.RequestData<Statement>(this);
 
@@ -96,18 +97,18 @@ namespace InCollege.Client.UI.MainUI
         async void MainWindow_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.F5)
+            {
+                IsEnabled = false;
                 await UpdateData();
+                IsEnabled = true;
+                Focus();
+            }
         }
 
         async void EditItem_Click(object sender, RoutedEventArgs e)
         {
             if (StatementsLV.SelectedItem != null)
-            {
-                EditStatementDialog.AddMode = false;
-                EditStatementDialog.Statement = (Statement)StatementsLV.SelectedItem;
-                await EditStatementDialog.UpdateData();
-                EditStatementDialog.IsOpen = true;
-            }
+                await EditStatementDialog.Show((Statement)StatementsLV.SelectedItem, false, false, false);
         }
 
         async void RemoveItem_Click(object sender, RoutedEventArgs e)
@@ -139,22 +140,7 @@ namespace InCollege.Client.UI.MainUI
 
         async void AddButton_Click(object sender, RoutedEventArgs e)
         {
-            await AddStatement();
-        }
-
-        async Task<int> AddStatement()
-        {
-            var statement = new Statement();
-            //Attention                    'HERE. It cannot be optimized that way, you thought about.
-            int id = statement.ID = int.Parse(await NetworkUtils.ExecuteDataAction<Statement>(this, statement, DataAction.Save));
-            statement.IsLocal = false;
-            EditStatementDialog.Statement = statement;
-            EditStatementDialog.AddMode = true;
-            EditStatementDialog.StatementResultsLV.ItemsSource = null;
-            EditStatementDialog.GroupCB.Visibility = Visibility.Collapsed;
-            await EditStatementDialog.UpdateData();
-            EditStatementDialog.IsOpen = true;
-            return id;
+            await AddStatement(false, false);
         }
 
         async void EditStatementDialog_OnSave(object sender, RoutedEventArgs e)
@@ -195,43 +181,94 @@ namespace InCollege.Client.UI.MainUI
             await NetworkUtils.RemoveWhere<StatementAttestationType>(null, (nameof(StatementAttestationType.StatementID), statement.ID));
             await NetworkUtils.RemoveWhere<CommissionMember>(null, (nameof(CommissionMember.StatementID), statement.ID));
             await NetworkUtils.RemoveWhere<StatementResult>(null, (nameof(StatementResult.StatementID), statement.ID));
+            await NetworkUtils.RemoveWhere<RePass>(null, (nameof(RePass.StatementID), statement.ID));
         }
 
         async void GenerateComplexStatementItem_Click(object sender, RoutedEventArgs e)
         {
-            //At least one is selected
-            if (StatementsLV.SelectedItem != null)
-            {
-                int statementID = await AddStatement();
-                var data = await NetworkUtils.RequestData<StatementResult>(this, strict: true, orAll: true, preserveContext: true, column: null,
-                                                                           whereParams: StatementsLV.SelectedItems
-                                                                           .OfType<Statement>()
-                                                                           .Select(c => (
-                                                                            name: nameof(StatementResult.StatementID),
-                                                                            value: (object)c.ID))
-                                                                           .ToArray());
-                var distinctor = new DistinctByStudentAndSubject();
-
-                data.Sort((c1, c2) => c1.StatementResultDate > c2.StatementResultDate ? 1 : -1);
-
-                data.Select(c => (studentID: c.StudentID, subjectID: c.SubjectID))
-                    .Select(c => data.LastOrDefault(currentResult => currentResult.StudentID == c.studentID &&
-                                                                     currentResult.SubjectID == c.subjectID))
-                    .Distinct(distinctor)
-                    .ToList()
-                    .ForEach(async c =>
-                    {
-                        c.StatementID = statementID;
-                        c.IsLocal = true;
-                        await NetworkUtils.ExecuteDataAction<StatementResult>(null, c, DataAction.Save);
-                    });
-                await EditStatementDialog.UpdateData();
-            }
+            await GenerateStatement(true, false, 1);
         }
 
-        void GenerateTotalStatementItem_Click(object sender, RoutedEventArgs e)
+        async void GenerateTotalStatementItem_Click(object sender, RoutedEventArgs e)
         {
+            await GenerateStatement(false, true, 4);
+        }
 
+        async Task GenerateStatement(bool complexMode, bool totalMode, int strangeThreshold)
+        {
+            if (StatementsLV.SelectedItems.Count > 0)
+                if (StatementsLV.SelectedItems.Count > strangeThreshold ||
+                    MessageBox.Show($"Имеет ли смысл? Меньше {strangeThreshold + 1} ведомостей.", "Странная операция", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    var statement = await AddStatement(complexMode, totalMode, false);
+                    if (statement != null && statement.ID != -1)
+                    {
+                        Cursor = Cursors.Wait;
+
+                        var data = await NetworkUtils.RequestData<StatementResult>(this, strict: true, orAll: true, preserveContext: true, column: null,
+                                                                                   whereParams: StatementsLV.SelectedItems
+                                                                                   .OfType<Statement>()
+                                                                                   .Select(c => (
+                                                                                    name: nameof(StatementResult.StatementID),
+                                                                                    value: (object)c.ID))
+                                                                                   .ToArray());
+                        var distinctor = new DistinctByStudentAndSubject();
+                        //Latest - at the end
+                        data.Sort((c1, c2) => c1.StatementResultDate > c2.StatementResultDate ? 1 : c1.StatementResultDate < c2.StatementResultDate ? -1 : 0);
+
+                        //Get empty student-subject intersections, fill with blanks.
+                        //Get last student-subject couples, distinct by distinctor(student-subject)->
+                        //set owner as just created statement->
+                        //make 'local' to tell the system to re-create it on server side->
+                        //save on server->
+                        //update data in opened by adding new statement->
+                        var studentIDs = data.Select(c => c.StudentID).Distinct();
+                        var subjectIDs = data.Select(c => c.SubjectID).Distinct();
+
+                        foreach (var studentID in studentIDs)
+                            foreach (var subjectID in subjectIDs)
+                                if (!data.Any(c => c.StudentID == studentID && c.SubjectID == subjectID))
+                                    await NetworkUtils.ExecuteDataAction<StatementResult>(null, new StatementResult
+                                    {
+                                        StatementID = statement.ID,
+                                        StudentID = studentID,
+                                        SubjectID = subjectID,
+                                        MarkValue = (sbyte)TechnicalMarkValue.Blank,
+                                        StatementResultDate = null,
+                                        TicketNumber = -1
+                                    }, DataAction.Save);
+
+                        foreach (var current in data.Select(c => (studentID: c.StudentID, subjectID: c.SubjectID))
+                                                    .Select(c => data.LastOrDefault(currentResult => currentResult.StudentID == c.studentID &&
+                                                                                                     currentResult.SubjectID == c.subjectID))
+                                                    .Distinct(distinctor))
+                        {
+                            current.StatementID = statement.ID;
+                            current.IsLocal = true;
+                            await NetworkUtils.ExecuteDataAction<StatementResult>(null, current, DataAction.Save);
+                        }
+
+                        await EditStatementDialog.UpdateData();
+                        Cursor = Cursors.Arrow;
+
+                        await EditStatementDialog.Show(statement, true, complexMode, totalMode);
+                    }
+                }
+        }
+
+        async Task<Statement> AddStatement(bool generatedComplexMode, bool generatedTotalMode, bool showDialog = true)
+        {
+            if (!(generatedComplexMode && generatedTotalMode))
+            {
+                var statement = new Statement();
+                //Attention                    'HERE. It cannot be optimized that way, you thought about.
+                statement.ID = int.Parse(await NetworkUtils.ExecuteDataAction<Statement>(this, statement, DataAction.Save));
+                statement.IsLocal = false;
+                if (showDialog)
+                    await EditStatementDialog.Show(statement, true, generatedComplexMode, generatedTotalMode);
+                return statement;
+            }
+            else return null;
         }
     }
 
