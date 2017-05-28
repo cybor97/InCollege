f544458f950544886602dafc7578c7a10f6577e4
cybor <muhametshin.1997@gmail.com>
2017-05-29 02:07:31 +0300

    Added server-side app version.

--- InCollege.Server/CommonVariables.cs
+++ InCollege.Server/CommonVariables.cs
@@ -6,5 +6,6 @@ namespace InCollege.Server
     class CommonVariables
     {
         public static string DBLocation = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "InCollege.Server", "InCollege.db");
+        public static string Version = "v1.0 (API 1.0)";
     }
 }
