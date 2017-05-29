45ae09182099d4d4c30af7caf5783c1c870d2505
cybor <muhametshin.1997@gmail.com>
2017-05-29 23:58:01 +0300

    Implemented About UI modes.

--- InCollege/UI/AboutWindow.xaml.cs
+++ InCollege/UI/AboutWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
@@ -10,25 +11,56 @@ namespace InCollege.Client.UI
         public AboutWindow()
         {
             InitializeComponent();
-            AboutContentTB.Text = Properties.Resources.About;
+            ShowInfo(InfoTag("Base"));
         }
 
         int Counter = 0;
         async void TitleTB_Click(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            if (++Counter == 10)
+            if (++Counter >= 10)
             {
                 TitleTB.Foreground = Brushes.Red;
                 TitleTB.IsEnabled = false;
-                await Task.Run(() => Thread.Sleep(1000));
-                if (Counter > 15)
+                if (Counter > 20)
                 {
                     Counter = 0;
                     MessageBox.Show("Замечательно! Вы нашли ее! Забирайте! ?assign=1");
                 }
+                else await Task.Run(() => Thread.Sleep(Counter == 10 ? 1000 : 300));
                 TitleTB.Foreground = Brushes.Lime;
                 TitleTB.IsEnabled = true;
             }
+        }
+
+        bool ComponentsMode = false;
+        private void ComponentsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ComponentsMode = !ComponentsMode;
+            ShowInfo(InfoTag(ComponentsMode ? "Components" : "Base"));
+            ComponentsButton.Content = ComponentsMode ? "Основная информация" : "Сторонние компоненты";
+            ContactsButton.Visibility = ComponentsMode ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        bool ContactsMode = false;
+        private void ContactsButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContactsMode = !ContactsMode;
+            ShowInfo(InfoTag(ContactsMode ? "Contacts" : "Base"));
+            ContactsButton.Content = ContactsMode ? "Основная информация" : "Контакты";
+            ComponentsButton.Visibility = ContactsMode ? Visibility.Collapsed : Visibility.Visible;
+
+        }
+
+        void ShowInfo((string open, string close) tag)
+        {
+            AboutContentTB.Text = Properties.Resources.About
+                                          .Split(new[] { tag.open }, StringSplitOptions.RemoveEmptyEntries)[1]
+                                          .Split(new[] { tag.close }, StringSplitOptions.RemoveEmptyEntries)[0];
+        }
+
+        (string open, string close) InfoTag(string name)
+        {
+            return ($"[{name}]", $"[/{name}]");
         }
     }
 }
