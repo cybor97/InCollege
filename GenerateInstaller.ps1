7a5e02f61555297cdc7d263e2b9cba8be9ad262b
cybor <muhametshin.1997@gmail.com>
2017-06-19 09:27:34 +0300

    _

--- GenerateInstaller.ps1
+++ GenerateInstaller.ps1
@@ -1,7 +1,7 @@
 MSBuild
-Compress-Archive -Path .\InCollege\bin\Debug\* -DestinationPath .\InCollege.Installer\Resources\InCollege_Client -Update
+Compress-Archive -Path .\InCollege\bin\Release\* -DestinationPath .\InCollege.Installer\Resources\InCollege_Client -Update
 mv .\InCollege.Installer\Resources\InCollege_Client.zip .\InCollege.Installer\Resources\InCollege_Client -force
-Compress-Archive -Path .\InCollege.Server\bin\Debug\* -DestinationPath .\InCollege.Installer\Resources\InCollege_Server -Update
+Compress-Archive -Path .\InCollege.Server\bin\Release\* -DestinationPath .\InCollege.Installer\Resources\InCollege_Server -Update
 mv .\InCollege.Installer\Resources\InCollege_Server.zip .\InCollege.Installer\Resources\InCollege_Server -force
 MSBuild .\InCollege.Installer\InCollege.Installer.csproj
-.\InCollege.Installer\bin\Debug\InCollege.Installer.exe
+.\InCollege.Installer\bin\Release\InCollege.Installer.exe
